99c747f61d7712d56d4cde2795ef7d8b306a174e
roditnepal <nrodit@gmail.com>
2025-02-01 01:43:36 -0800

    Database connection establish

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const userRoute = require('./routes/userRoute');
 
-const app = express()
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Routes Middleware
+app.use('/api/users', userRoute);
+
+//Routes
+app.get("/", (req, res) => {
+    res.send('Home Page');
+});
+
+console.log(process.env.MONGODB_URI);
+
 
 const PORT = process.env.PORT || 5000;
 
